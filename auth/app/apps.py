d06096a69b9cfbf6c64a15dae6f5f1a4acd970a6
lunacircle4 <lunacircle4@likelion.org>
2021-07-09 22:52:10 +0900

    feat: floweryroad v2 api

---
+++ auth/app/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class AppConfig(AppConfig):
+    # name is app folder name
+    name = 'app'
+
+    # label is unique seperator on database
+    label = 'floweryroad_auth' 
---
+++ auth/config/environments/production.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from .base import *
+import os
+
+DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+ROOT_URLCONF = 'config.urls.production'
+
+CORS_ORIGIN_WHITELIST = [
+    os.environ['CORS']
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
